eece0c6e5a8244110408ba58314c5f09e0d183a4
RedK22 <ritwik22k@gmail.com>
2025-07-14 00:43:44 +0530

    05-AgentKit setup

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
-import {inngest} from "./client";
+import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
-  {id: "hello-world"},
-  {event: "test/hello.world"},
-  async ({event, step}) => {
-    await step.sleep("wait-a-moment", "1s");
-    return {message: `Hello ${event.data.email}!`};
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.JS developer.You write readable, maintainable code. You write in simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await codeAgent.run(
+      // Text here
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    return { output };
   }
 );
